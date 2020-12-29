5cef7163ad20a62bb524e7ed77741b26dbaa5595
sankalp chari <sankalpchari@gmail.com>
2020-12-29 10:57:14 +0530

    initial commit

---
+++ src/components/main.jsx
@@ -0,0 +1,21 @@
+import react,{Component} from 'react'
+
+class Main extends Component{
+
+
+
+
+
+render(){
+    return (
+                <>
+                <h1>Hiiiiii</h1>
+                </>
+    );
+}
+
+
+
+} 
+
+export default Main
